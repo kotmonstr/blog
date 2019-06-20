55b1c35140a3373037a946f9b5ed53f61554895c
Konstantin Sychyov <ksychyov@alvioneurope.ru>
2019-06-20 15:06:39 +0300

    add task

--- README.txt
+++ README.txt
@@ -1,1 +1,9 @@
-## Blog
+# Blog
+
+## Задачи
+
+### 1. Разбить <body> на три файлика( header.php, content.php, footer.php ) с помощью языка php
+### 2.
+### 3.
+### 4.
+### 5.
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Blog</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>Blog</title>
+    </head>
 
-<body>
-Content of the document......
-</body>
+    <body>
+        Content of the document......
+    </body>
 
 </html>
